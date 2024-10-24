d5aaaa2bbe1ff214ce64895c0f7f34d070e61f49
Mohamed <karam.hamam95@gmail.com>
2024-10-24 22:08:21 +0300

    fix-issues

--- client/src/app/pages/signout-page/signout-page.component.ts
+++ client/src/app/pages/signout-page/signout-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signout-page',
@@ -8,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './signout-page.component.scss'
 })
 export class SignoutPageComponent {
+
+
+  constructor(private router: Router) {
+
+  }
+  signout() {
+
+    localStorage.removeItem('token');
+    this.router.navigate(['/']);
+  }
+
 
 }
